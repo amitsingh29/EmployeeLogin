d921e1994c2e3d56fa885ec8c993009c0938cad2
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-23 21:09:02 +0530

    [Amit] Add. Added Employee.cs inside EmployeeModel

---
+++ EmployeeCRUD/EmployeeModel/Employee.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCRUD.EmployeeModel
+{
+    public class Employee
+    {
+        private string id;
+
+        /// <summary>
+        /// The full name
+        /// </summary>
+        private string fullName;
+
+        /// <summary>
+        /// The age
+        /// </summary>
+        private string age;
+
+        /// <summary>
+        /// The email identifier
+        /// </summary>
+        private string emailId;
+
+        /// <summary>
+        /// The work experience
+        /// </summary>
+        private int workExp;
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public string Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        /// <value>
+        /// The full name.
+        /// </value>
+        public string FullName
+        {
+            get
+            {
+                return this.fullName;
+            }
+
+            set
+            {
+                this.fullName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>
+        /// The age.
+        /// </value>
+        public string Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the email identifier.
+        /// </summary>
+        /// <value>
+        /// The email identifier.
+        /// </value>
+        public string EmailId
+        {
+            get
+            {
+                return this.emailId;
+            }
+
+            set
+            {
+                this.emailId = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the work experience.
+        /// </summary>
+        /// <value>
+        /// The work experience.
+        /// </value>
+        public int WorkExp
+        {
+            get
+            {
+                return this.workExp;
+            }
+
+            set
+            {
+                this.workExp = value;
+            }
+        }
+    }
+}
