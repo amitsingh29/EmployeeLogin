a83ad7ac4bf955212549ee57277b2e773cc3d9e5
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-24 12:46:08 +0530

    [Amit] Add. added AppDbContext class

---
+++ EmployeeCRUD/EmployeeModel/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCRUD.EmployeeModel
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
