708ee0e88ae34e298aa6df84ccd2adae38319fe1
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-24 23:00:05 +0530

    [Amit] Refactor. made changes in appSettings.json

--- EmployeeCRUD/Startup.cs
+++ EmployeeCRUD/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Repository.Context;
 
 namespace EmployeeCRUD
 {
@@ -25,7 +28,13 @@ namespace EmployeeCRUD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<UserDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UserDbConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(d =>
+            {
+                d.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +43,8 @@ namespace EmployeeCRUD
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(d => { d.SwaggerEndpoint("/swagger/v1/swagger.json", "myapi v1"); });
             }
             else
             {
