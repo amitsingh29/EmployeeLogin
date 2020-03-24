0a4ec8c2e82493e006b58b9fefafc829632c9da5
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-24 19:37:44 +0530

    [Amit] Add. Employee class added to Model

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
-namespace EmployeeCRUD.EmployeeModel
+namespace Model
 {
     public class Employee
     {
+        /// <summary>
+        /// The identifier
+        /// </summary>
         private string id;
 
         /// <summary>
