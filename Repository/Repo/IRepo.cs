09c70ccbb3c282434666a20f55263597786faea2
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-26 14:24:51 +0530

    [Amit] Refactor. made changes in IRepo

--- Repository/Repo/IRepo.cs
+++ Repository/Repo/IRepo.cs
@@ -5,33 +5,12 @@ using System.Text;
 
 namespace Repository.Repo
 {
-    interface IRepo
+    public interface IRepo
     {
-        /// <summary>
-        /// Adds the employee.
-        /// </summary>
-        /// <param name="employee">The employee.</param>
-        /// <returns>Boolean value</returns>
+        
         bool AddEmployee(Employee employee);
-
-        /// <summary>
-        /// Deletes the employee.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <returns>Boolean value</returns>
         bool DeleteEmployee(int id);
-
-        /// <summary>
-        /// Gets all employee.
-        /// </summary>
-        /// <returns>Get list</returns>
         List<Employee> GetAllEmployee();
-
-        /// <summary>
-        /// Updates the employee.
-        /// </summary>
-        /// <param name="employee">The employee.</param>
-        /// <returns>Boolean value</returns>
         bool UpdateEmployee(Employee employee);
     }
 }
---
+++ Repository/Repo/IRepoImpl.cs
@@ -0,0 +1,30 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Repo
+{
+    public class IRepoImpl : IRepo
+    {
+        public bool AddEmployee(Employee employee)
+        {
+            
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+           
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            
+        }
+
+        public bool UpdateEmployee(Employee employee)
+        {
+            
+        }
+    }
+}
