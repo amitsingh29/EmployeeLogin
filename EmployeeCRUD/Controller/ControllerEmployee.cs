a2781643bd8a05a282913829364c76d0341ade69
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-27 16:22:40 +0530

    [Amit] Add. adding ControllerEmployee in Controller

---
+++ EmployeeCRUD/Controller/ControllerEmployee.cs
@@ -0,0 +1,72 @@
+﻿using Manager;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCRUD.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControllerEmployee : ControllerBase
+    {
+        public readonly IManager manager;
+        public ControllerEmployee(IManager manager)
+        {
+            this.manager = manager;
+        }
+
+        [Route("AddEmployee")]
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Model.Employee employee)
+        {
+            var result = await this.manager.AddEmployee(employee);
+            if (result == 1)
+            {
+                return this.Ok(employee);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+
+        [Route("GetEmployee")]
+        [HttpGet]
+        public Employee GetEmployee(int id)
+        {
+            return this.manager.GetEmployee(id);
+        }
+
+        [Route("UpdateEmployee")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(Employee employeeChanges)
+        {
+            var result = await this.manager.UpdateEmployee(employeeChanges);
+            if (result == 1)
+            {
+                return this.Ok(employeeChanges);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        public Employee DeleteEmployee(int id)
+        {
+            return this.manager.DeleteEmployee(id);
+        }
+
+        [Route("GetAllEmployee")]
+        [HttpGet]
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return this.manager.GetAllEmployees();
+        }
+    }
+}
