9765010338c8f6ca1687a28abf98f8e07c6b3478
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-28 10:55:05 +0530

    [Amit] Refactor. added Stylecop in UserDatabaseContext

--- Repository/Context/UserDatabaseContext.cs
+++ Repository/Context/UserDatabaseContext.cs
@@ -1,16 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Model;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Employee.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace Repository.Context
 {
+    using Microsoft.EntityFrameworkCore;
+    using Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// UserDatabaseContext class 
+    /// </summary>
     public class UserDatabaseContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDatabaseContext"/> class.
+        /// </summary>
+        /// <param name="options">The UserDatabaseContext</param>
         public UserDatabaseContext(DbContextOptions<UserDatabaseContext> options) : base(options)
         {
         }
+
+        /// <summary>
+        /// Gets and sets the Employee
+        /// </summary>
         public DbSet<Employee> Employee { get; set; }
     }
 }
