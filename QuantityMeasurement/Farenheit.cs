0282c20ed0e69963dbf6872c19f7719fb7669360
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-03 20:10:54 +0530

    [Amit] Add. adding Farenheit.cs

---
+++ QuantityMeasurement/Farenheit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class Farenheit
+    {
+        public double farenheit;
+
+        public Farenheit()
+        {
+        }
+
+        public Farenheit(double farenheit)
+        {
+            this.farenheit = farenheit;
+        }
+
+        public double EqualsValue()
+        {
+            return this.farenheit;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
