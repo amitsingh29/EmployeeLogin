56ebabffe55df7471a84b17b21b3843ecca5a75b
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-03 20:15:00 +0530

    [Amit] Add. Adding Gram.cs

---
+++ QuantityMeasurement/Gram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class Gram
+    {
+        public int gram;
+
+        public Gram()
+        {
+        }
+
+        public Gram(int gram)
+        {
+            this.gram = gram;
+        }
+
+        public int EqualsValue()
+        {
+            return this.gram;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
