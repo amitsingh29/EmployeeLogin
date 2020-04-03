a6b724271e306c0cd6bf94ddf5bb63e46fddc818
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-03 20:38:28 +0530

    [Amit] Add. Adding Kilogram.cs

---
+++ QuantityMeasurement/Kilogram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class Kilogram
+    {
+        public int kg;
+
+        public Kilogram()
+        {
+        }
+
+        public Kilogram(int kg)
+        {
+            this.kg = kg;
+        }
+
+        public int EqualsValue()
+        {
+            return this.kg;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
